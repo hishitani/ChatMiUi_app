63a6da2f777bc9cd4f37b82171133ce360315d2e
Hishitani <artem.kinforg.12@gmail.com>
2024-01-07 03:44:47 -0800

    Fixed new blocks

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -30,7 +30,7 @@ async function Page() {
 										alt='user_logo'
 										width={20}
 										height={20}
-										className='rounded-full object-cover'
+										className='rounded-full object-cover h-5'
 									/>
 									<p className='!text-small-regular text-light-1'>
 										<span className='mr-1 text-primary-500'>
